5c0eda7126f5e08b02bd177b09d3040ff43477ee
solitary-reaper15 <anoushka.2815@gmail.com>
2025-11-20 20:04:23 -0500

    added config to store keys + links

---
+++ src/Config.java
@@ -0,0 +1,15 @@
+/**
+ * Config : just stores the API keys and anything external links
+  /)/)
+ ( . .)
+ ( づ♡
+ */
+
+public class Config {
+    // Add your API keys and configuration here
+    public static final String OPEN_ROUTER_API_KEY = "your_actual_api_key";
+    public static final String NINJA_API_KEY = "yenuWSGz+AZ0FMtDkHfNbw==jJFN4ywYAj6fJZhh";
+    public static final String NINJA_API_URL = "https://api.api-ninjas.com/v1/animals?name=";
+
+
+}
