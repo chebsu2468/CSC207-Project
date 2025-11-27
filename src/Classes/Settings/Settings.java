6b2d07cf3bef34e42d43395d6b0b6aa471c1fdb2
bsu <cheb.su@mil.utoronto.ca>
2025-11-27 00:10:28 -0500

    made some variable changes to TextSetting

--- src/Classes/Settings/Settings.java
+++
@@ -1,9 +0,0 @@
-package Classes.Settings;
-
-public abstract class Settings {
-    /**
-     * Creating settings for the UI, changes different UI feature,
-     * such as font, text size, and color
-     * Implements memory property by read and write data from a CSV file
-     */
-}
